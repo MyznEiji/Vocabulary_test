28221cadac1161a23d1bae80e703c2296f71ec2e
MyznEiji <em711th@icloud.com>
2019-02-24 18:22:27 +0900

    feature : choice all or weekly

--- models/teacher.py
+++ models/teacher.py
@@ -35,7 +35,9 @@ class EnglishTeacher(Teacher):
     """Handle data model on English."""
 
     parts_of_speech_list = ["Noun", "Pronoun", "Adjective", "Verb", "Adverb", "Preposition", "Conjunction", "Interjection", "Phrase"]
-    csv_path = "./csv/hoge.csv"
+    all_csv_path = "./csv/all.csv"
+    weekly_csv_path = "./csv/weekly.csv"
+
     csv_columns = "english,japanese,parts_of_speech"
     csv_columns_list = ["english" ,"japanese" ,"parts_of_speech"]
 
@@ -82,9 +84,9 @@ class EnglishTeacher(Teacher):
 
 
     # 新しい単語を追加する
-    def write_new_vocabulary(self, new_vocabulary):
+    def write_new_vocabulary(self, new_vocabulary, csv_file):
         # if there is not csv
-        with open(self.csv_path, "r+") as f:
+        with open(csv_file, "r+") as f:
             line = f.read().split('\n')[0]
             print(line)
             if line != self.csv_columns:
@@ -92,7 +94,7 @@ class EnglishTeacher(Teacher):
                 f.write(self.csv_columns + '\n')
     
     
-        with open(self.csv_path, 'a') as c_file:
+        with open(csv_file, 'a') as c_file:
             writer = csv.DictWriter(c_file, fieldnames=self.csv_columns_list, lineterminator='\n')
             writer.writerow({"english": new_vocabulary['en'], "japanese": new_vocabulary['ja'], "parts_of_speech": new_vocabulary["parts_of_speech"]})
     
@@ -109,16 +111,16 @@ class EnglishTeacher(Teacher):
 
 
 
-    def practice(self):
+    def practice(self, csv_file):
         """for practice vocabulary"""
 
         # Vocabularyの数を取得
-        f = open(self.csv_path)
+        f = open(csv_file)
         num_lines = sum(1 for line in f)
         f.close
 
         # Vocabularyのcsvデータを配列にトランスフォーム
-        vocaburaly_array = self.make_array(self.csv_path)
+        vocaburaly_array = self.make_array(csv_file)
 
         # 乱数を0からnum_lines-2(colomunsの行があるのと、配列は0から始まるから)までで10個生成して、vocabularyを取得する
         for i, question_num in enumerate(random.randint(0, num_lines-2) for n in range(10)):
@@ -182,10 +184,22 @@ class EnglishTeacher(Teacher):
             }))
 
             if int(todo) == 0:
-                self.practice()
+                # all.csvかweekly.csvnのどちらに書き込むか選択
+                template = console.get_template(
+                    'which_all_or_weekly.txt', self.speak_color)
+                want_to_do_num = int(input(template.substitute()))
+
+                # 0の場合all_csv、1の場合weekly_csvを読み込みpracticeをする
+                if want_to_do_num == 0:
+                    self.practice(self.all_csv_path)
+                elif want_to_do_num == 1:
+                    self.practice(self.weekly_csv_path)
+
+
             elif int(todo) == 1:
                 new_vocabulary = self.input_new_vocabulary()
-                self.write_new_vocabulary(new_vocabulary)
+                self.write_new_vocabulary(new_vocabulary, self.weekly_csv_path)
+                
             elif int(todo) == 2:
                 break
             else:
